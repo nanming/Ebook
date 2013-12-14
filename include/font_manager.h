27868cfad4f82be8b7c4f07c98e74e89250fe70b
nanming <584114384@qq.com>
2013-12-14 17:25:09 +0800

    ready write font function

---
+++ include/font_manager.h
@@ -0,0 +1,23 @@
+#ifndef _FONT_MANAGER_H
+#define _FONT_MANAGER_H
+
+typedef struct FontBitMap {
+	int iXLeft;
+	int iYTop;
+	int iXMax;
+	int iYMax;
+	int iBpp;
+	int iPitch;
+	int iCurOriginX;
+	int iCurOriginY;
+	int iNextOriginX;
+	int iNextOriginY;
+	unsigned char *pucBuffer;	
+}T_FontBitMap, *PT_FontBitMap;
+
+typedef struct FontOpr {
+	char *name;
+	int (*FontInit)();
+	int (*GetCodeBitMap)();
+	struct FontOpr *next;
+}T_FontOpr, *PT_FontOpr;
